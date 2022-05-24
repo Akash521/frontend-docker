450a0c4e7c1d0d1079915191f1b986ef2c2a4b81
Akash521 <akash.br@pivotchain.com>
2022-05-24 16:21:46 +0530

    changes added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hey Saurbh and Akash !123'))
+app.get('/', (req, res) => res.send('Hey Saurbh and Akash'))
 app.listen(3000, () => console.log('Server ready'))
